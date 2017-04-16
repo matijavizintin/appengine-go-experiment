0b0851194b5fa89578955f10ed35c58fbb75b1c9
matijavizintin <matija.vizintin@gmail.com>
2017-04-16 15:21:57 +0200

    dummy work to put load on machines

--- docker/server.go
+++ docker/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -16,10 +18,27 @@ func main() {
 type indexHandler struct{}
 
 func (i indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	doSomeCpuIntensiveWork()
+
 	w.WriteHeader(http.StatusOK)
 
 	hostname, _ := os.Hostname()
 	w.Write([]byte(fmt.Sprintf("Hello from %s", hostname)))
+}
+
+func doSomeCpuIntensiveWork() {
+	slice := []int{}
+
+	for i := 0; i < 10000000; i++ {
+		slice = append(slice, 10000000-i)
+	}
+	sort.Ints(slice)
+
+	sum := 0.
+	for i := 0; i < 1000000; i++ {
+		sqrt := math.Sqrt(float64(i))
+		sum += sqrt
+	}
 }
 
 type healthCheckHandler struct{}
