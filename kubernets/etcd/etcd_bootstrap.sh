d80b02f5bdfcaba583184f63e456bd511779a0e8
matijavizintin <matija.vizintin@gmail.com>
2017-05-06 16:57:19 +0200

    kubernetes - etcd bootstrap

--- kubernets/etcd/etcd_bootstrap.sh
+++ kubernets/etcd/etcd_bootstrap.sh
@@ -5,7 +5,6 @@ HOST1=10.132.0.3
 HOST2=10.132.0.4
 
 PRIVATE_IPV4=$(curl -sw "\n" --header "Metadata-Flavor:Google" "http://metadata/computeMetadata/v1/instance/network-interfaces/0/ip")
-PUBLIC_IPV4=$(curl -sw "\n" --header "Metadata-Flavor:Google" "http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip")
 
 NAME=""
 case ${PRIVATE_IPV4} in
@@ -26,10 +25,10 @@ docker stop etcd
 docker rm etcd
 docker run -d --net=host --name etcd quay.io/coreos/etcd:v3.1.7 /usr/local/bin/etcd \
  --name ${NAME} \
- --advertise-client-urls http://${PUBLIC_IPV4}:2379 \
- --listen-client-urls http://${PUBLIC_IPV4}:2379 \
+ --advertise-client-urls http://${PRIVATE_IPV4}:2379 \
+ --listen-client-urls http://${PRIVATE_IPV4}:2379 \
  --initial-advertise-peer-urls http://${PRIVATE_IPV4}:2380 \
- --listen-peer-urls http://${PUBLIC_IPV4}:2380 \
+ --listen-peer-urls http://${PRIVATE_IPV4}:2380 \
  --initial-cluster-token etcd-cluster-1 \
  --initial-cluster etcd0=http://${HOST0}:2380,etcd1=http://${HOST1}:2380,etcd2=http://${HOST2}:2380 \
  --initial-cluster-state new
