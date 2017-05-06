39fc0bc0e4c0a8dc609be4eda9614a8f97144b6c
matijavizintin <matija.vizintin@gmail.com>
2017-05-06 16:07:43 +0200

    kubernetes - etcd bootstrap

--- kubernets/etcd/etcd_bootstrap.sh
+++ kubernets/etcd/etcd_bootstrap.sh
@@ -24,11 +24,12 @@ if [ -z "${NAME}" ]; then exit 1; fi
 
 docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
  --name etcd quay.io/coreos/etcd \
- -name ${NAME} \
- -advertise-client-urls http://${PUBLIC_IPV4}:2379,http://${PUBLIC_IPV4}:4001 \
- -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
- -initial-advertise-peer-urls http://${PRIVATE_IPV4}:2380 \
- -listen-peer-urls http://0.0.0.0:2380 \
- -initial-cluster-token etcd-cluster-1 \
- -initial-cluster etcd0=http://${HOST0}:2380,etcd1=http://${HOST1}:2380,etcd2=http://${HOST2}:2380 \
- -initial-cluster-state new
+ --entrypoint=/usr/local/bin/etcd \
+ --name ${NAME} \
+ --advertise-client-urls http://${PUBLIC_IPV4}:2379,http://${PUBLIC_IPV4}:4001 \
+ --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+ --initial-advertise-peer-urls http://${PRIVATE_IPV4}:2380 \
+ --listen-peer-urls http://0.0.0.0:2380 \
+ --initial-cluster-token etcd-cluster-1 \
+ --initial-cluster etcd0=http://${HOST0}:2380,etcd1=http://${HOST1}:2380,etcd2=http://${HOST2}:2380 \
+ --initial-cluster-state new
