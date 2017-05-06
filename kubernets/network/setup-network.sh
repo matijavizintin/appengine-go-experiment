c645861599d8d3c79f7afd72e4df86c807c025d0
matijavizintin <matija.vizintin@gmail.com>
2017-05-06 21:32:44 +0200

    fix setup-network

--- kubernets/network/setup-network.sh
+++ kubernets/network/setup-network.sh
@@ -2,8 +2,9 @@
 
 MASTER_NODE=10.132.0.2
 
-ssh ${MASTER_NODE} 'sudo mkdir /etc/flannel'
-scp options.env ${MASTER_NODE}:/etc/flannel/options.env
+ssh ${MASTER_NODE} 'sudo mkdir -p /etc/flannel'
+scp options.env ${MASTER_NODE}:
+ssh ${MASTER_NODE} 'sudo mv options.env /etc/flannel/'
 ssh ${MASTER_NODE} 'sudo sed -i -e s/ADVERTISE_IP/${COREOS_PRIVATE_IPV4}/g /etc/flannel/options.env'
 
 ssh ${MASTER_NODE} 'sudo mkdir -p /etc/systemd/system/flanneld.service.d'
