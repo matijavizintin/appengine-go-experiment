5ee87bf65465546e3f6759b4640b9eded70deb57
matijavizintin <matija.vizintin@gmail.com>
2017-05-07 11:30:03 +0200

    kube pods

--- kubernets/stage2/kube-pods/setup-pods.sh
+++ kubernets/stage2/kube-pods/setup-pods.sh
@@ -2,8 +2,8 @@
 
 # setup api server pod
 cp kube-apiserver.yaml kube-apiserver.yaml.copy
-sed -i -e "s/ETCD_ENDPOINTS/${ETCD_ENDPOINTS}/g" kube-apiserver.yaml.copy
-sed -i -e "s/SERVICE_IP_RANGE/${SERVICE_IP_RANGE}/g" kube-apiserver.yaml.copy
+sed -i -e "s|ETCD_ENDPOINTS|${ETCD_ENDPOINTS}|g" kube-apiserver.yaml.copy
+sed -i -e "s|SERVICE_IP_RANGE|${SERVICE_IP_RANGE}|g" kube-apiserver.yaml.copy
 
 ssh ${MASTER_HOST} 'sudo mkdir -p /etc/kubernetes/manifests'
 scp kube-apiserver.yaml.copy ${MASTER_HOST}:kube-apiserver.yaml
