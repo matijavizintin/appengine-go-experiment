de8f9b902e45668a34777a5507f2c8c888adc3fb
matijavizintin <matija.vizintin@gmail.com>
2017-05-07 11:06:54 +0200

    updated bash scripts

--- kubernets/stage2/kube-pods/setup-pods.sh
+++ kubernets/stage2/kube-pods/setup-pods.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # setup api server
+cp kube-apiserver.yaml kube-apiserver.yaml.copy
+sed -i -e s/ETCD_ENDPOINTS/${ETCD_ENDPOINTS}/g kube-apiserver.yaml.copy
+sed -i -e s/SERVICE_IP_RANGE/${SERVICE_IP_RANGE}/g kube-apiserver.yaml.copy
+
 ssh ${MASTER_HOST} 'sudo mkdir -p /etc/kubernetes/manifests'
-scp kube-apiserver.yaml ${MASTER_HOST}:
+scp kube-apiserver.yaml.copy ${MASTER_HOST}:kube-apiserver.yaml
 ssh ${MASTER_HOST} 'sudo mv kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml'
-ssh ${MASTER_HOST} 'sudo sed -i -e s/ETCD_ENDPOINTS/${ETCD_ENDPOINTS}/g /etc/kubernetes/manifests/kube-apiserver.yaml'
-ssh ${MASTER_HOST} 'sudo sed -i -e s/SERVICE_IP_RANGE/${SERVICE_IP_RANGE}/g /etc/kubernetes/manifests/kube-apiserver.yaml'
-ssh ${MASTER_HOST} 'sudo sed -i -e s/ADVERTISE_IP/${COREOS_PRIVATE_IPV4}/g /etc/kubernetes/manifests/kube-apiserver.yaml'
+
+rm kube-apiserver.yaml.copy
--- kubernets/stage2/kubelet/setup-kubelet.sh
+++ kubernets/stage2/kubelet/setup-kubelet.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-scp kubelet.service ${MASTER_HOST}:
+copy kubelet.service kubelet.service.copy
+sed -i -e s/DNS_SERVICE_IP/${DNS_SERVICE_IP}/g kubelet.service.copy
+
+scp kubelet.service.copy ${MASTER_HOST}:kubelet.service
 ssh ${MASTER_HOST} 'sudo mv kubelet.service /etc/systemd/system/kubelet.service'
-ssh ${MASTER_HOST} 'sudo sed -i -e s/NETWORK_PLUGIN/${NETWORK_PLUGIN}/g /etc/systemd/system/kubelet.service'
-ssh ${MASTER_HOST} 'sudo sed -i -e s/DNS_SERVICE_IP/${DNS_SERVICE_IP}/g /etc/systemd/system/kubelet.service'
+
+rm kubelet.service.copy
