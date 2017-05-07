fa4c7091d81be6a77984f1b83241f684b71b3557
matijavizintin <matija.vizintin@gmail.com>
2017-05-07 15:36:54 +0200

    setup network fix

--- kubernets/stage4-master/setup/network/setup-network.sh
+++ kubernets/stage4-master/setup/network/setup-network.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+cp options.env options.env.copy
+sed -i -e "s|ADVERTISE_IP|${MASTER_HOST}|g" options.env.copy
+sed -i -e "s|FLANNELD_ETCD_ENDPOINTS=ETCD_ENDPOINTS|FLANNELD_ETCD_ENDPOINTS=${ETCD_ENDPOINTS}|g" options.env.copy
+
 ssh ${MASTER_HOST} 'sudo mkdir -p /etc/flannel'
 scp options.env ${MASTER_HOST}:
 ssh ${MASTER_HOST} 'sudo mv options.env /etc/flannel/'
-ssh ${MASTER_HOST} 'sudo sed -i -e s/ADVERTISE_IP/${COREOS_PRIVATE_IPV4}/g /etc/flannel/options.env'
-ssh ${MASTER_HOST} 'sudo sed -i -e s/FLANNELD_ETCD_ENDPOINTS=ETCD_ENDPOINTS/FLANNELD_ETCD_ENDPOINTS=${ETCD_ENDPOINTS}/g /etc/flannel/options.env'
+
+rm options.env.copy
 
 ssh ${MASTER_HOST} 'sudo mkdir -p /etc/systemd/system/flanneld.service.d'
 scp 40-ExecStartPre-symlink.conf ${MASTER_HOST}:
--- kubernets/stage5-workers/setup/network/setup-network.sh
+++ kubernets/stage5-workers/setup/network/setup-network.sh
@@ -2,11 +2,15 @@
 
 for host in ${WORKER1_IP} ${WORKER2_IP}
 do
+    cp options.env options.env.copy
+    sed -i -e "s|ADVERTISE_IP|${host}|g" options.env.copy
+    sed -i -e "s|FLANNELD_ETCD_ENDPOINTS=ETCD_ENDPOINTS|FLANNELD_ETCD_ENDPOINTS=${ETCD_ENDPOINTS}|g" options.env.copy
+
     ssh ${host} 'sudo mkdir -p /etc/flannel'
     scp options.env ${host}:
     ssh ${host} 'sudo mv options.env /etc/flannel/'
-    ssh ${host} 'sudo sed -i -e s/ADVERTISE_IP/${COREOS_PRIVATE_IPV4}/g /etc/flannel/options.env'
-    ssh ${host} 'sudo sed -i -e s/FLANNELD_ETCD_ENDPOINTS=ETCD_ENDPOINTS/FLANNELD_ETCD_ENDPOINTS=${ETCD_ENDPOINTS}/g /etc/flannel/options.env'
+
+    rm options.env.copy
 
     ssh ${host} 'sudo mkdir -p /etc/systemd/system/flanneld.service.d'
     scp 40-ExecStartPre-symlink.conf ${host}:
