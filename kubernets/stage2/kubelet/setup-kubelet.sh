201633045dc0fdf9df7f181c95936c9e82004a21
matijavizintin <matija.vizintin@gmail.com>
2017-05-07 10:57:12 +0200

    kube pods

--- kubernets/stage2/kubelet/setup-kubelet.sh
+++ kubernets/stage2/kubelet/setup-kubelet.sh
@@ -2,3 +2,5 @@
 
 scp kubelet.service ${MASTER_HOST}:
 ssh ${MASTER_HOST} 'sudo mv kubelet.service /etc/systemd/system/kubelet.service'
+ssh ${MASTER_HOST} 'sudo sed -i -e s/NETWORK_PLUGIN/${NETWORK_PLUGIN}/g /etc/systemd/system/kubelet.service'
+ssh ${MASTER_HOST} 'sudo sed -i -e s/DNS_SERVICE_IP/${DNS_SERVICE_IP}/g /etc/systemd/system/kubelet.service'
