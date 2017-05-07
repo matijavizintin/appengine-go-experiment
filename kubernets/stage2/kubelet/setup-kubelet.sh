1400160bdfdccc98f015e392262811bd19e54d27
matijavizintin <matija.vizintin@gmail.com>
2017-05-07 11:25:12 +0200

    kube pods

--- kubernets/stage2/kubelet/setup-kubelet.sh
+++ kubernets/stage2/kubelet/setup-kubelet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp kubelet.service kubelet.service.copy
-sed -i 's/DNS_SERVICE_IP/${DNS_SERVICE_IP}/g' kubelet.service.copy
+sed -i -e "s/DNS_SERVICE_IP/${DNS_SERVICE_IP}/g" kubelet.service.copy
 
 scp kubelet.service.copy ${MASTER_HOST}:kubelet.service
 ssh ${MASTER_HOST} 'sudo mv kubelet.service /etc/systemd/system/kubelet.service'
