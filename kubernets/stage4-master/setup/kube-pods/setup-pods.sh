922bfd0b10fb2ccf5e4e59c58fc3921e66337932
matijavizintin <matija.vizintin@gmail.com>
2017-05-08 20:11:32 +0200

    bug fix

--- kubernets/stage4-master/setup/kube-pods/setup-pods.sh
+++ kubernets/stage4-master/setup/kube-pods/setup-pods.sh
@@ -4,10 +4,10 @@
 cp kube-apiserver.yaml kube-apiserver.yaml.copy
 sed -i -e "s|ETCD_ENDPOINTS|${ETCD_ENDPOINTS}|g" kube-apiserver.yaml.copy
 sed -i -e "s|SERVICE_IP_RANGE|${SERVICE_IP_RANGE}|g" kube-apiserver.yaml.copy
-sed -i -e "s|ADVERTISE_ADDRESS|${COREOS_PRIVATE_IPV4}|g" kube-apiserver.yaml.copy
 
 ssh ${MASTER_HOST} 'sudo mkdir -p /etc/kubernetes/manifests'
 scp kube-apiserver.yaml.copy ${MASTER_HOST}:kube-apiserver.yaml
+ssh ${MASTER_HOST} 'sed -i -e "s|ADVERTISE_ADDRESS|${COREOS_PRIVATE_IPV4}|g" kube-apiserver.yaml'
 ssh ${MASTER_HOST} 'sudo mv kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml'
 
 rm kube-apiserver.yaml.copy
